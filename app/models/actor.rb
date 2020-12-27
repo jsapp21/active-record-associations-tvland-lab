28520f8c2d4fe699fbc048da5b2beb820ce13020
James Sapp <jameseisapp@gmail.com>
2020-12-26 18:26:13 -0600

    commit tree 8eef7cd04bca611d046310842ccbcc39f74adf67

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    #returns the first and last name of an actor
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters.collect do |character|
+        "#{character.name} - #{character.show.name}"
+    end
+  end
+
 end
